d29c7c46ad32f3a54169ba7ce86f86956e6cb088
Krzysztof Maliński <malinkrzychu@gmail.com>
2023-05-05 19:11:40 +0200

    Added hahaha to scipt

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("hahahaha");
     }
 }
